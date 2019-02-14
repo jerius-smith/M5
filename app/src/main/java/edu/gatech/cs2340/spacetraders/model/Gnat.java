59c29e4dddac01b681b3c2dc7c04e937456e0e52
Shrey Amin <samin36@gatech.edu>
2019-02-14 10:00:24 -0500

    Modified the player and the Gnat class

--- app/src/main/java/edu/gatech/cs2340/spacetraders/model/Gnat.java
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/model/Gnat.java
@@ -1,7 +1,7 @@
 package edu.gatech.cs2340.spacetraders.model;
 
 public class Gnat extends Ship {
-    public Gnat(String name) {
-        super(name);
+    public Gnat() {
+        super("Gnat");
     }
 }
--- app/src/main/java/edu/gatech/cs2340/spacetraders/model/Player.java
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/model/Player.java
@@ -8,5 +8,15 @@ public class Player {
     private int credits;
     private Ship ship;
 
+    public Player(String name) {
+        this(name, Difficulty.BEGINNER);
+    }
 
+    public Player(String name, Difficulty preferredDifficulty) {
+        this.name = name;
+        this.preferredDifficulty = preferredDifficulty;
+        this.skills = Skills.values();
+        this.ship = new Gnat();
+        this.credits = 1000;
+    }
 }
