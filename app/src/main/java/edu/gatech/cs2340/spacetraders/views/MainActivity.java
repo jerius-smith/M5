db2f5b6be5d68a999dde03394914bc340349684b
Shrey Amin <samin36@gatech.edu>
2019-02-14 20:51:24 -0500

    Click the start button launches configuration activity

--- app/src/main/java/edu/gatech/cs2340/spacetraders/views/MainActivity.java
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/views/MainActivity.java
@@ -1,11 +1,13 @@
 package edu.gatech.cs2340.spacetraders.views;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
 
 import edu.gatech.cs2340.spacetraders.R;
 
@@ -16,6 +18,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageButton startButton = (ImageButton) findViewById(R.id.istart_new_button);
+        startButton.setOnClickListener((view) -> {
+            Intent intent = new Intent(MainActivity.this, ConfigurationAcitivty.class);
+            startActivity(intent);
+        });
         //Git test
         //New comment
     }
