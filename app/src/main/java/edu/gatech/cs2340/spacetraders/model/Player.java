27ea93db130868834417edc0727115048fe39e52
Shrey Amin <samin36@gatech.edu>
2019-02-14 22:08:17 -0500

    Bound the views from content_configuration to ConfigurationActivity.java

--- app/src/main/java/edu/gatech/cs2340/spacetraders/model/Player.java
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/model/Player.java
@@ -7,6 +7,7 @@ public class Player {
     private Difficulty preferredDifficulty;
     private int credits;
     private Ship ship;
+    private int availablePoints;
 
     public Player(String name) {
         this(name, Difficulty.BEGINNER);
@@ -18,5 +19,54 @@ public class Player {
         this.skills = Skills.values();
         this.ship = new Gnat();
         this.credits = 1000;
+        this.availablePoints = Skills.MAX_POINTS;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Skills[] getSkills() {
+        return skills;
+    }
+
+    public void setSkills(Skills[] skills) {
+        this.skills = skills;
+    }
+
+    public Difficulty getPreferredDifficulty() {
+        return preferredDifficulty;
+    }
+
+    public void setPreferredDifficulty(Difficulty preferredDifficulty) {
+        this.preferredDifficulty = preferredDifficulty;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    public Ship getShip() {
+        return ship;
+    }
+
+    public void setShip(Ship ship) {
+        this.ship = ship;
+    }
+
+    public int getAvailablePoints() {
+        return availablePoints;
+    }
+
+    public void setAvailablePoints(int availablePoints) {
+        this.availablePoints = availablePoints;
     }
 }
--- app/src/main/java/edu/gatech/cs2340/spacetraders/views/ConfigurationActivity.java
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/views/ConfigurationActivity.java
@@ -2,13 +2,49 @@ package edu.gatech.cs2340.spacetraders.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
 import edu.gatech.cs2340.spacetraders.R;
+import edu.gatech.cs2340.spacetraders.model.Difficulty;
+import edu.gatech.cs2340.spacetraders.model.Player;
 
 public class ConfigurationActivity extends AppCompatActivity {
+
+    private TextView playerName;
+    private TextView difficulty;
+    private TextView skillHeader;
+    private TextView points;
+    private EditText nameInput;
+    private Spinner difficultySpinner;
+
+    private Player player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuration);
+
+        playerName = (TextView) findViewById(R.id.name_header);
+        difficulty = (TextView) findViewById(R.id.difficulty_header);
+        skillHeader = (TextView) findViewById(R.id.skill_header);
+        points = (TextView) findViewById(R.id.points_header);
+        nameInput = (EditText) findViewById(R.id.name_input);
+        difficultySpinner = (Spinner) findViewById(R.id.difficulty_spinner);
+
+
+        ArrayAdapter<Difficulty> adapter = new ArrayAdapter<Difficulty>(this,
+                                                           android.R.layout.simple_spinner_item,
+                                                           Difficulty.values());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        difficultySpinner.setAdapter(adapter);
+        difficultySpinner.setSelection(0);
+
+        points.setText(player.getAvailablePoints());
+        points.setOnClickListener((view) -> {
+            player.setAvailablePoints(player.getAvailablePoints() - 1);
+        });
     }
 }
