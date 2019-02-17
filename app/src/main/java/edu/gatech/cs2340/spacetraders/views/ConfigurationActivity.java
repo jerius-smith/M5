cbbf98432c0134e7207ce46d7d652960f76b62c5
jerius.smith <jerius.smith@gmail.com>
2019-02-16 19:49:15 -0500

    updating config screen with start button and functionality

--- app/src/main/java/edu/gatech/cs2340/spacetraders/views/ConfigurationActivity.java
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/views/ConfigurationActivity.java
@@ -1,26 +1,29 @@
 package edu.gatech.cs2340.spacetraders.views;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import edu.gatech.cs2340.spacetraders.R;
 import edu.gatech.cs2340.spacetraders.model.Difficulty;
 import edu.gatech.cs2340.spacetraders.model.Player;
+import edu.gatech.cs2340.spacetraders.model.Skills;
 
 public class ConfigurationActivity extends AppCompatActivity {
 
-    private TextView playerName;
-    private TextView difficulty;
-    private TextView skillHeader;
     private TextView points;
     private EditText nameInput;
     private Spinner difficultySpinner;
+
+    private EditText[] skills = new EditText[4];
 
     private Player player;
 
@@ -30,12 +33,14 @@ public class ConfigurationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuration);
 
-        playerName = (TextView) findViewById(R.id.name_header);
-        difficulty = (TextView) findViewById(R.id.difficulty_header);
-        skillHeader = (TextView) findViewById(R.id.skill_header);
-        points = (TextView) findViewById(R.id.points_header);
-        nameInput = (EditText) findViewById(R.id.name_input);
-        difficultySpinner = (Spinner) findViewById(R.id.difficulty_spinner);
+        points = findViewById(R.id.skill_points);
+        nameInput = findViewById(R.id.name_input);
+        difficultySpinner = findViewById(R.id.difficulty_spinner);
+
+        skills[0] = findViewById(R.id.pilot_skills);
+        skills[1] = findViewById(R.id.fighter_skills);
+        skills[2] = findViewById(R.id.trader_skills);
+        skills[3] = findViewById(R.id.engineer_skills);
 
 
         ArrayAdapter<Difficulty> adapter = new ArrayAdapter<Difficulty>(this,
@@ -49,8 +54,37 @@ public class ConfigurationActivity extends AppCompatActivity {
 
         Log.d("PLAYER_INFO", "" + player.getAvailablePoints());
         points.setText(String.format("%d", player.getAvailablePoints()));
-        points.setOnClickListener((view) -> {
-            player.setAvailablePoints(player.getAvailablePoints() - 1);
+        points.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                player.setAvailablePoints(player.getAvailablePoints() - 1);
+            }
         });
     }
+
+    /**
+     * Button handler for the start button
+     *
+     * @param view the button that was pressed
+     *
+     */
+    public void onAddPressed(View view) {
+        Log.d("Edit", "Start New Game Pressed");
+
+        Skills[] skillsArr = {Skills.PILOT, Skills.FIGHTER, Skills.TRADER, Skills.ENGINEER};
+
+        for (int i = 0; i < skillsArr.length; i++) {
+            skillsArr[i].setPoints(Integer.parseInt(skills[i].getText().toString()));
+        }
+
+
+        player.setName(nameInput.getText().toString());
+        player.setPreferredDifficulty((Difficulty) difficultySpinner.getSelectedItem());
+        player.setSkills(skillsArr);
+
+//        Intent intent = new Intent(ConfigurationActivity.this, WelcomeActivity.class);
+//        startActivity(intent);
+
+    }
+
 }
--- app/src/main/java/edu/gatech/cs2340/spacetraders/views/MainActivity.java
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/views/MainActivity.java
@@ -2,10 +2,7 @@ package edu.gatech.cs2340.spacetraders.views;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -18,10 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageButton startButton = (ImageButton) findViewById(R.id.istart_new_button);
-        startButton.setOnClickListener((view) -> {
-            Intent intent = new Intent(MainActivity.this, ConfigurationActivity.class);
-            startActivity(intent);
+        ImageButton startNewButton = findViewById(R.id.istart_new_button);
+        startNewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ConfigurationActivity.class);
+                startActivity(intent);
+            }
         });
         //Git test
         //New comment
